f1a1e1fd4f761593c5bb8c3ea275186e2f1f5c90
Michael Militar <mikeymilitar10@gmail.com>
2021-01-25 14:46:46 -0500

    added url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://rocky-ocean-49854.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
